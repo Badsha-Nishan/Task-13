bbbca7de08aa39528d6a684a1bc113fc8a3afa2b
Badsha-Nishan <badshanisan14@gmail.com>
2026-02-01 14:40:24 +0600

    added task 4

--- index.js
+++ index.js
@@ -25,15 +25,30 @@
 // Task-3
 // Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
 
-function make_avg(arr) {
-  const size = arr.length;
-  let avg = 0;
-  for (const val of arr) {
-    avg += val;
+// function make_avg(arr) {
+//   const size = arr.length;
+//   let avg = 0;
+//   for (const val of arr) {
+//     avg += val;
+//   }
+//   return avg / size;
+// }
+
+// const arr = [2, 4, 6];
+// const result = make_avg(arr);
+// console.log(result);
+
+// Task-4
+// Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+
+const str = "01000101000101010100001";
+function count_zero(binaryStr) {
+  let count = 0;
+  for (const val of binaryStr) {
+    if (val === "0") count++;
   }
-  return avg / size;
+  return count;
 }
 
-const arr = [2, 4, 6];
-const result = make_avg(arr);
+const result = count_zero(str);
 console.log(result);
