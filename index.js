8296bb9e8082d09e8ea81a0bdf7a87bdaa503b62
Badsha-Nishan <badshanisan14@gmail.com>
2026-02-01 14:31:26 +0600

    added task 3

--- index.js
+++ index.js
@@ -11,13 +11,29 @@
 // Task-2
 // Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
 
-function number(num) {
-  if (num % 2 !== 0) {
-    return num * 2;
-  } else {
-    return num / 2;
+// function number(num) {
+//   if (num % 2 !== 0) {
+//     return num * 2;
+//   } else {
+//     return num / 2;
+//   }
+// }
+
+// const result = number(10);
+// console.log(result);
+
+// Task-3
+// Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+
+function make_avg(arr) {
+  const size = arr.length;
+  let avg = 0;
+  for (const val of arr) {
+    avg += val;
   }
+  return avg / size;
 }
 
-const result = number(10);
+const arr = [2, 4, 6];
+const result = make_avg(arr);
 console.log(result);
